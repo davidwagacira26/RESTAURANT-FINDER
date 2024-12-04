e6ac5ff249a8f951b48520d344b2c5f56dd9c67e
David Wagacira <davidngese26@gmail.com>
2024-12-04 14:05:46 +0300

    update recommendations.js

--- public/javascript/restaurantdata.js
+++ public/javascript/restaurantdata.js
@@ -3,7 +3,7 @@ export const restaurants = [
         name: "Botanica - Kitchen and Gin Bar",
         type: "International",
         rating: 4.4,
-        location: "Nairobi",
+        location: "Westlands",
         image: "/assets/images/Botanica.jpg",
         detailsLink: "/public/views/botanica.html"
     },
@@ -11,7 +11,7 @@ export const restaurants = [
         name: "Bambino",
         type: "Italian",
         rating: 4.2,
-        location: "Nairobi",
+        location: "Westlands",
         image: "/assets/images/bambino.jpg",
         detailsLink: "/public/views/bambino.html"
     },
@@ -19,135 +19,135 @@ export const restaurants = [
         name: "INTI",
         type: "Japanese",
         rating: 4.5,
-        location: "Nairobi",
+        location: "Westlands",
         image: "/assets/images/INTI3.jpg",
         detailsLink: "/public/views/inti.html"
     },
     {
         name: "Canopy Cafe",
-        type: "Cafe",
-        rating: 4.0,
-        location: "Nairobi",
+        type: "International",
+        rating: 4.1,
+        location: "Kilimani",
         image: "/assets/images/canopy.jpg",
-        detailsLink: "/public/views/canopy-cafe.html"
+        detailsLink: "/public/views/canopy.html"
     },
     {
         name: "Cafe Cassia",
-        type: "Cafe",
-        rating: 4.0,
-        location: "Nairobi",
+        type: "International",
+        rating: 4.7,
+        location: "Karen",
         image: "/assets/images/cafecassia.jpg",
         detailsLink: "/public/views/cafecassia.html"
     },
     {
-        name: "ARTISAN BLEND CAFE",
-        type: "Cafe",
-        rating: 4.2,
-        location: "Nairobi",
+        name: "Artisan Blend Cafe",
+        type: "International",
+        rating: 4.8,
+        location: "Mombasa Road",
         image: "/assets/images/artisanblend.jpg",
         detailsLink: "/public/views/artisan-blend-cafe.html"
     },
     {
         name: "Mercado - Mexican Kitchen and Gin Bar",
         type: "Mexican",
-        rating: 4.5,
-        location: "Nairobi",
+        rating: 4.4,
+        location: "Westlands",
         image: "/assets/images/mercado.jpg",
-        detailsLink: "/public/views/mercado-mexican-kitchen-and-gin-bar.html"
+        detailsLink: "/public/views/mercado.html"
     },
     {
         name: "Pili Restaurant",
-        type: "African",
-        rating: 4.3,
-        location: "Nairobi",
+        type: "International",
+        rating: 4.8,
+        location: "Westlands",
         image: "/assets/images/pili.jpg",
-        detailsLink: "/public/views/pili-restaurant.html"
+        detailsLink: "/public/views/pili.html"
     },
     {
         name: "Crave Restaurant",
         type: "International",
-        rating: 3.9,
-        location: "Nairobi",
+        rating: 4.8,
+        location: "Kilimani",
         image: "/assets/images/crave.jpg",
-        detailsLink: "/public/views/crave-restaurant.html"
+        detailsLink: "/public/views/crave.html"
     },
     {
         name: "Spring Noshery",
         type: "International",
-        rating: 4.0,
-        location: "Nairobi",
+        rating: 4.4,
+        location: "Westlands",
         image: "/assets/images/springnoshery2.jpg",
-        detailsLink: "/public/views/spring-noshery.html"
+        detailsLink: "/public/views/springnoshery.html"
     },
     {
-        name: "Yunion - Brunch and cafe",
-        type: "Cafe",
-        rating: 4.3,
-        location: "Nairobi",
+        name: "Yunion - Brunch and Cafe",
+        type: "International",
+        rating: 4.0,
+        location: "Westlands",
         image: "/assets/images/yunion.jpg",
-        detailsLink: "/public/views/yunion-brunch-and-cafe.html"
+        detailsLink: "/public/views/yunion.html"
     },
     {
         name: "Kahani Restaurant",
         type: "Indian",
-        rating: 4.3,
-        location: "Nairobi",
+        rating: 3.9,
+        location: "Limuru Road",
         image: "/assets/images/kahani.jpg",
-        detailsLink: "/public/views/kahani-restaurant.html"
+        detailsLink: "/public/views/kahani.html"
     },
     {
         name: "Slate - Kitchen and Bar",
         type: "International",
-        rating: 4.1,
-        location: "Nairobi",
+        rating: 4.4,
+        location: "Mkungu Cl",
         image: "/assets/images/slate.jpg",
-        detailsLink: "/public/views/slate-kitchen-and-bar.html"
+        detailsLink: "/public/views/slate.html"
     },
     {
         name: "Brew Bistro - Fortis",
         type: "International",
-        rating: 4.2,
-        location: "Nairobi",
+        rating: 4.8,
+        location: "Westlands",
         image: "/assets/images/brewbistro.jpg",
-        detailsLink: "/public/views/brew-bistro-fortis.html"
+        detailsLink: "/public/views/brewbistro.html"
     },
     {
         name: "270° Restaurant",
         type: "International",
-        rating: 4.2,
-        location: "Nairobi",
+        rating: 4.3,
+        location: "Westlands",
         image: "/assets/images/270°2.jpg",
-        detailsLink: "/public/views/270-restaurant.html"
+        detailsLink: "/public/views/270°-restaurant.html"
     },
     {
         name: "Harvest Restaurant",
         type: "International",
-        rating: 4.3,
-        location: "Nairobi",
+        rating: 4.5,
+        location: "Village Market",
         image: "/assets/images/harvest.jpg",
-        detailsLink: "/public/views/harvest-restaurant.html"
+        detailsLink: "/public/views/harvestrestaurant.html"
     },
     {
         name: "Meko Restaurant",
         type: "International",
-        rating: 4.1,
-        location: "Nairobi",
+        rating: 3.9,
+        location: "Peponi Rd",
         image: "/assets/images/meko.jpg",
         detailsLink: "/public/views/meko-restaurant.html"
     },
     {
         name: "Jiko Restaurant",
-        type: "Kenyan",
-        rating: 4.4,
-        location: "Nairobi",
+        type: "African",
+        rating: 4.5,
+        location: "Tribe Hotel",
         image: "/assets/images/jiko.jpg",
         detailsLink: "/public/views/jiko-restaurant.html"
     },
     {
         name: "Talisman Restaurant",
         type: "International",
-        rating: 4.7,
-        location: "Nairobi",
+        rating: 4.6,
+        location: "Ngong Rd",
         image: "/assets/images/talisman.jpg",
         detailsLink: "/public/views/talisman-restaurant.html"
     },
@@ -155,15 +155,15 @@ export const restaurants = [
         name: "Inca Restaurant",
         type: "Peruvian",
         rating: 4.4,
-        location: "Nairobi",
+        location: "The Social House",
         image: "/assets/images/inca.jpg",
         detailsLink: "/public/views/inca-restaurant.html"
     },
     {
         name: "Karel T-Lounge",
-        type: "Cafe",
-        rating: 4.2,
-        location: "Nairobi",
+        type: "Mediterranean",
+        rating: 4.4,
+        location: "Village Market",
         image: "/assets/images/kareltlounge4.jpg",
         detailsLink: "/public/views/karel-t-lounge.html"
     },
@@ -171,47 +171,47 @@ export const restaurants = [
         name: "Ezo Restaurant",
         type: "Japanese",
         rating: 4.4,
-        location: "Nairobi",
+        location: "42 Muthithi Rd",
         image: "/assets/images/ezo2.jpg",
         detailsLink: "/public/views/ezo-restaurant.html"
     },
     {
-        name: "Nairobi street kitchen",
+        name: "Nairobi Street Kitchen",
         type: "International",
-        rating: 4.1,
-        location: "Nairobi",
+        rating: 4.5,
+        location: "Mpaka Rd",
         image: "/assets/images/nsk.jpg",
         detailsLink: "/public/views/nairobi-street-kitchen.html"
     },
     {
         name: "Fogo Gaucho",
-        type: "Steakhouse",
-        rating: 4.2,
-        location: "Nairobi",
+        type: "Brazillian",
+        rating: 4.6,
+        location: "Viking House",
         image: "/assets/images/fogogaucho.jpg",
         detailsLink: "/public/views/fogo-gaucho.html"
     },
     {
         name: "Le Terrazza Italian Restaurant",
         type: "Italian",
-        rating: 4.4,
+        rating: 4.7,
         location: "Nairobi",
         image: "/assets/images/laterazza.jpg",
         detailsLink: "/public/views/le-terrazza-italian-restaurant.html"
     },
     {
         name: "Upepo Restaurant",
-        type: "International",
-        rating: 4.3,
-        location: "Nairobi",
+        type: "African",
+        rating: 4.2,
+        location: "Peponi Rd",
         image: "/assets/images/upepo2.jpg",
         detailsLink: "/public/views/upepo-restaurant.html"
     },
     {
-        name: "Mawimbi Seafood",
-        type: "Seafood",
-        rating: 4.5,
-        location: "Nairobi",
+        name: "Mawimbi Seafood Restaurant",
+        type: "International",
+        rating: 4.6,
+        location: "Kijabe St",
         image: "/assets/images/mawimbi2.jpg",
         detailsLink: "/public/views/mawimbi-seafood.html"
     },
@@ -219,97 +219,81 @@ export const restaurants = [
         name: "Lucca Restaurant",
         type: "Italian",
         rating: 4.6,
-        location: "Nairobi",
-        image: "/placeholder.svg?height=200&width=300",
+        location: "Chiromo Ln",
+        image: "/assets/images/lucca.jpg",
         detailsLink: "/public/views/lucca-restaurant.html"
     },
     {
         name: "Chophouse Nairobi",
-        type: "Steakhouse",
+        type: "African",
         rating: 4.5,
-        location: "Nairobi",
-        image: "/placeholder.svg?height=200&width=300",
+        location: "Elgon Rd",
+        image: "/assets/images/the-chop-house.jpg",
         detailsLink: "/public/views/chophouse-nairobi.html"
     },
     {
         name: "Tatu Restaurant",
         type: "International",
         rating: 4.6,
-        location: "Nairobi",
-        image: "/placeholder.svg?height=200&width=300",
+        location: "Harry Thuku Rd",
+        image: "/assets/images/taturestaurant.jpg",
         detailsLink: "/public/views/tatu-restaurant.html"
     },
     {
         name: "Copper the Urban Grill",
-        type: "Steakhouse",
+        type: "International",
         rating: 4.4,
-        location: "Nairobi",
-        image: "/placeholder.svg?height=200&width=300",
+        location: "154 James Gichuru Rd",
+        image: "/assets/images/coppertheurbangrill2.jpg",
         detailsLink: "/public/views/copper-the-urban-grill.html"
     },
     {
         name: "The Larder Restaurant",
         type: "International",
-        rating: 4.2,
-        location: "Nairobi",
-        image: "/placeholder.svg?height=200&width=300",
+        rating: 3.9,
+        location: "Elgon Rd",
+        image: "/assets/images/thelarder.jpg",
         detailsLink: "/public/views/the-larder-restaurant.html"
     },
     {
-        name: "Tamborone Restaurant",
-        type: "African",
-        rating: 4.3,
-        location: "Nairobi",
-        image: "/placeholder.svg?height=200&width=300",
-        detailsLink: "/public/views/tamborone-restaurant.html"
-    },
-    {
-        name: "Luca Restaurant",
-        type: "Italian",
-        rating: 4.5,
-        location: "Nairobi",
-        image: "/placeholder.svg?height=200&width=300",
-        detailsLink: "/public/views/luca-restaurant.html"
+        name: "Tambourin Restaurant",
+        type: "Lebanese",
+        rating: 4.4,
+        location: "Chiromo Ln",
+        image: "/assets/images/tambourin.jpg",
+        detailsLink: "/public/views/tambourin-restaurant.html"
     },
     {
         name: "Graze Steakhouse",
-        type: "Steakhouse",
+        type: "American",
         rating: 4.5,
-        location: "Nairobi",
-        image: "/placeholder.svg?height=200&width=300",
+        location: "Woodvale Grove",
+        image: "/assets/images/grazesteakhouse.jpg",
         detailsLink: "/public/views/graze-steakhouse.html"
     },
     {
-        name: "Seven seafood and grill",
+        name: "Seven Seafood & Grill",
         type: "Seafood",
-        rating: 4.6,
-        location: "Nairobi",
-        image: "/placeholder.svg?height=200&width=300",
+        rating: 4.5,
+        location: "Waiyaki Wy",
+        image: "/assets/images/sevenseafoodandgrill.jpg",
         detailsLink: "/public/views/seven-seafood-and-grill.html"
     },
     {
         name: "Hero Restaurant",
         type: "Asian",
-        rating: 4.3,
-        location: "Nairobi",
-        image: "/placeholder.svg?height=200&width=300",
+        rating: 4.6,
+        location: "Limuru Rd",
+        image: "/assets/images/herorestaurant.jpg",
         detailsLink: "/public/views/hero-restaurant.html"
     },
     {
         name: "Urban Eatery",
         type: "International",
-        rating: 4.3,
-        location: "Nairobi",
-        image: "/placeholder.svg?height=200&width=300",
+        rating: 4.4,
+        location: "Chiromo Rd",
+        image: "/assets/images/urbaneatery.jpg",
         detailsLink: "/public/views/urban-eatery.html"
     },
-    {
-        name: "Mama Nilishe",
-        type: "African",
-        rating: 4.1,
-        location: "Nairobi",
-        image: "/placeholder.svg?height=200&width=300",
-        detailsLink: "/public/views/mama-nilishe.html"
-    }
 ];
 
