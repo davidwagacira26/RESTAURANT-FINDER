df3377831a6bb73eb60bbb38d5f87ff20a7d95f4
David Wagacira <davidngese26@gmail.com>
2024-12-08 11:20:12 +0300

    changes

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'nairobi-feast-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/public/stylesheets/styles.css',
+  '/public/javascript/cookie-consent.js',
+  '/images/icon-192x192.png',
+  '/images/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
