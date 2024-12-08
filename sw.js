cd909db56af0e2a0f0a78d3147f11afa54d37739
David Wagacira <davidngese26@gmail.com>
2024-12-08 11:33:08 +0300

    changes

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'nairobi-feast-v1';
 const urlsToCache = [
   '/',
-
+  '/index.html',
   '/public/stylesheets/styles.css',
   '/public/javascript/cookie-consent.js',
   '/images/icon-192x192.png',
@@ -17,7 +17,41 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+
+        // Clone the response since it can only be consumed once
+        const responseToCache = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+
+        return response;
+      })
+      .catch(() => {
+        // If network request fails, try to get it from the cache
+        return caches.match(event.request);
+      })
+  );
+});
+
+// Clean up old caches when a new service worker takes over
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
